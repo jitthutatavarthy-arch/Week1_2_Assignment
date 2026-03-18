33b7b2d16bea501ed90c98e13c42f092ee56c7b3
jitthutatavarthy-arch <jitthutatavrthy@gmail.com>
2026-03-18 08:28:36 +0530

    Added level1 3rd problem

---
+++ convertmiles.java
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    float km = 10.8;
+    float miles;
+
+    miles = km * 1.6;
+
+    cout << "The distance " << km << " km in miles is " << miles;
+
+    return 0;
+}
