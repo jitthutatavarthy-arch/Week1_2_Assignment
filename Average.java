3f4e998065a090571345038b0ab1fe4db3bc6a53
jitthutatavarthy-arch <jitthutatavrthy@gmail.com>
2026-03-18 08:20:52 +0530

    Added level1 2nd problem

---
+++ Average.java
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int maths = 94;
+    int physics = 95;
+    int chemistry = 96;
+
+    float average;
+
+    average = (maths + physics + chemistry) / 3.0;
+
+    cout << "Sam's average mark in PCM is " << average;
+
+    return 0;
+}
